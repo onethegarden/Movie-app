7b4426f34a6e4417ce486d6799b609b872282657
onethegarden <onethegarden@gmail.com>
2020-02-27 20:27:34 +0900

    add,minus button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name,picture}){
-  return(
-  <div>
-    <h2>I like {name}</h2>
-    <img src={picture} alt ={name}/>
-  </div>
-  );
-}
-const foodILike=[
-  {
-    id : 1,
-    name : "kimchi",
-    image : "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
-  },
-  {
-    id :2,
-    name : "samgiopsal",
-    image : "https://ppss.kr/wp-content/uploads/2017/03/capture-20170323-162810.png"
-  },
-  {
-    id:3,
-    name : "chukumi",
-    image : "https://craftlog.com/m/i/5979130=s1280=h960"
+
+/*
+state
+React automatically is going excute render method function of my class component
+*/
+class App extends React.Component{
+  state = {
+    count : 0
   }
-];
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish =>(
-        <Food key ={dish.id} name ={dish.name} picture={dish.image}/>
-      ))}
-    </div>
-  );
+  add =()=>{
+    console.log("add");
+  };
+  minus =()=>{
+    console.log("minus");
+  };
+  render(){
+    return(
+      <div>
+        <h1>The number is  {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+      );
+  }
 }
 
 export default App;
