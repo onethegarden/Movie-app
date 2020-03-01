c1f639dc7e9eab2ba83c3442942a403143049d92
onethegarden <onethegarden@gmail.com>
2020-03-01 17:30:41 +0900

    add style

--- src/App.js
+++ src/App.js
@@ -34,12 +34,12 @@ class App extends React.Component{
       <section>
         {isLoading
           ? (
-            <div class="loader">
-              <span class="loader_text">Loading</span>
+            <div className="loader">
+              <span className="loader_text">Loading</span>
             </div>
           )
           : (
-            <div class="movies">
+            <div className="movies">
              {movies.map(movie=>(
             <Movie 
               key ={movie.id}
@@ -48,6 +48,7 @@ class App extends React.Component{
               title={movie.title} 
               summary={movie.summary} 
               poster={movie.medium_cover_image}
+              genres={movie.genres}
               />
             ))} 
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -2,22 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movies({id, year, title, summary, poster}){
-    return<div>
+function Movies({year, title, summary, poster, genres}){
+    return(
+    <div className="movie">
         <img src={poster} alt={title} title={title}/>
-        <div class="movie_data">
-            <h3 class="movie_title" >{title}</h3>
-            <h5 class="movie_year">{year}</h5>
-            <p class="movie_summary">{summary}</p>
+        <div className="movie_data">
+            <h3 className="movie_title" >{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <p className="movie_summary">{summary}</p>
+            <ul className="genres">
+                {genres.map((genre, index) => (
+                <li key={index} className="genres_genre">{genre}</li>
+            ))}
+            </ul>
+
         </div>
-    </div>;
+    </div>
+    );
 }
 Movies.propTypes={
     id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired
+    poster:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movies;
