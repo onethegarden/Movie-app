310f1a051261f6a3736bf0ea29c617f49bc5f78c
onethegarden <onethegarden@gmail.com>
2020-03-03 07:36:31 +0900

    getting ready for the router

--- src/App.js
+++ src/App.js
@@ -1,62 +1,6 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
-/*
-state
-React automatically is going excute render method function of my class component
 
-every time you call setState react going to call rerender function with new State
-
-construcor() when a component show up on the screen then, render()
-https//yts-proxy.now.sh/list_movies.json
-
-*/
-class App extends React.Component{
-  state={
-    isLoading : true,
-    movies: []
-  };
-  getMovies = async () =>{
-    const {
-      data:{
-        data:{ movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState( { movies, isLoading:false } );
-  };
-  componentDidMount(){
-      this.getMovies();
-  }
-  render(){
-    const { isLoading, movies } = this.state;
-    return (
-      <section>
-        {isLoading
-          ? (
-            <div className="loader">
-              <span className="loader_text">Loading</span>
-            </div>
-          )
-          : (
-            <div className="movies">
-             {movies.map(movie=>(
-            <Movie 
-              key ={movie.id}
-              id = {movie.id} 
-              year={movie.year} 
-              title={movie.title} 
-              summary={movie.summary} 
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-              />
-            ))} 
-          </div>
-          )
-        }
-      </section>
-    )        
-  }
+function App(){
+  return<span>something</span>;
 }
-
 export default App;
