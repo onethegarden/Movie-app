40a574adc6b749ab312cc6af60c056f8c15a90af
onethegarden <onethegarden@gmail.com>
2020-03-09 07:42:19 +0900

    tmp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import "./Navigation.css";
 
 function Naviation(){
     //link has to be inside router
     return (
-    <div class="navigation">
-        <Link class="nav" to="/">Home</Link>
+    <div className="navigation">
+        <Link className="nav" to="/">Home</Link>
         <span> | </span>
-        <Link class="nav" to="/about">About</Link>
+        <Link className="nav" to="/about">About</Link>
     </div>
     );
 }
